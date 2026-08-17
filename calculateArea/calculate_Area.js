63f74eae22002f9e27c82da1c11d6e6ae1b2baff
rayaishik <rayaishik321@gmail.com>
2026-08-17 08:02:24 -0400

    lab4p2

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,11 +2,6 @@ let length;
 let width;
 
 function calculateArea() {
-    length = parseFloat(document.getElementById('lenght').value);
-    width = parseFloat(document.getElementById('width').value);
-}
-
-function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
    
---
+++ groceryTracker/grocery_tracker.js
@@ -0,0 +1,11 @@
+let amount1, amount2, amount3;
+
+function calculateTotal() {
+    amount1 = parseFloat(document.getElementById('amount1').value);
+    amount2 = parseFloat(document.getElementById('amount2').value);
+    amount3 = parseFloat(document.getElementById('amount3').value);
+
+    let total = amount1 + amount2 + amount3;
+
+    document.getElementById('result').innerText = `The total amount is: $${total}`;
+}
